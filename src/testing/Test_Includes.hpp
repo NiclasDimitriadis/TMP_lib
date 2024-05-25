795796a71f92b83a2900f81d736bcb026a15a837
Niclas <n.dimitriadis@protonmail.com>
2024-05-25 15:39:09 +0200

    added function_signature functionality

--- src/testing/Test_Includes.hpp
+++ src/testing/Test_Includes.hpp
@@ -1,11 +1,13 @@
 #pragma once
 
 #include <algorithm>
+#include <functional>
 #include <iostream>
 #include <tuple>
 #include <type_traits>
 #include <variant>
 
+#include "function_signature.hpp"
 #include "helpers.hpp"
 #include "monoidal_class_template.hpp"
 #include "non_rep_combinations.hpp"
